38d2dc86ef13eea35bcd15b005216d2615fd563d
JomkhA <akkharaw.jomkhanthiphol.179@my.csun.edu>
2023-10-03 14:26:56 -0700

    first + last name instead of full name (signup)

--- app/src/main/java/com/example/loginscreen/SignUpActivity.java
+++ app/src/main/java/com/example/loginscreen/SignUpActivity.java
@@ -11,16 +11,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    EditText fullName, email, username, passwordEntered, passwordConfirm, phoneNumber;
+    EditText firstName, lastName, email, username, passwordEntered, passwordConfirm, phoneNumber;
     Button signupButton;
 
-    //Test test test for github
-    // Need to implement Firebase to store this registration information
+    // @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
 
-        fullName = findViewById(R.id.fullName);
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
         email = findViewById(R.id.email);
         username = findViewById(R.id.username);
         passwordEntered = findViewById(R.id.passwordEntered);
@@ -28,20 +28,22 @@ public class SignUpActivity extends AppCompatActivity {
         signupButton = findViewById(R.id.signupButton);
         phoneNumber = findViewById(R.id.phoneNumber);
 
-        fullName.setHorizontallyScrolling(true);
+        firstName.setHorizontallyScrolling(true);
+        lastName.setHorizontallyScrolling(true);
         username.setHorizontallyScrolling(true);
 
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = fullName.getText().toString();
+                String firstNameSignUp = firstName.getText().toString();
+                String lastNameSignUp = lastName.getText().toString();
                 String emailSignUp = email.getText().toString();
                 String passwordSignUp = passwordEntered.getText().toString();
                 String passwordSignUpConfirm = passwordConfirm.getText().toString();
                 String phoneSignUp = phoneNumber.getText().toString();
 
                 //Function for validation of inputted data
-                boolean check = validateInfo (name,emailSignUp,passwordSignUp,passwordSignUpConfirm, phoneSignUp);
+                boolean check = validateInfo (firstNameSignUp, lastNameSignUp, emailSignUp, passwordSignUp, passwordSignUpConfirm, phoneSignUp);
 
                 if (check == true) {
                     Toast.makeText(SignUpActivity.this, "SIGN UP SUCCESSFUL", Toast.LENGTH_SHORT).show(); //WE WOULD NEED TO ADVANCE USER TO HOME PAGE HERE
@@ -52,25 +54,35 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
     }
-    private Boolean validateInfo (String name, String emailSignUp, String passwordSignUp, String passwordSignUpConfirm, String phoneSignUp) {
-        if (name.length() == 0) {
-            fullName.requestFocus();
-            fullName.setError("FIELD CANNOT BE EMPTY");
+    private Boolean validateInfo (String firstNameSignUp, String lastNameSignUp, String emailSignUp, String passwordSignUp, String passwordSignUpConfirm, String phoneSignUp) {
+        if (firstNameSignUp.length() == 0) {
+            firstName.requestFocus();
+            firstName.setError("FIELD CANNOT BE EMPTY");
             return false;
         }
-        else if (!name.matches("[a-zA-Z]+")) {
-            fullName.requestFocus();
-            fullName.setError("ENTER ONLY ALPHABETICAL CHARACTER");
+        else if (!firstNameSignUp.matches("[a-zA-Z]+")) {
+            firstName.requestFocus();
+            firstName.setError("ENTER ONLY ALPHABETICAL CHARACTERS");
             return false;
         }
-        else if (email.length() == 0) {
+        if (lastNameSignUp.length() == 0) {
+            lastName.requestFocus();
+            lastName.setError("FIELD CANNOT BE EMPTY");
+            return false;
+        }
+        else if (!lastNameSignUp.matches("[a-zA-Z]+")) {
+            lastName.requestFocus();
+            lastName.setError("ENTER ONLY ALPHABETICAL CHARACTERS");
+            return false;
+        }
+        else if (emailSignUp.length() == 0) {
             email.requestFocus();
             email.setError("FIELD CANNOT BE EMPTY");
             return false;
         }
         else if (!emailSignUp.matches("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")) {
             email.requestFocus();
-            email.setError("ENTER VALID EMAIL");
+            email.setError("INVALID EMAIL");
             return false;
         }
         else if (phoneSignUp.length() == 0) {
@@ -78,14 +90,14 @@ public class SignUpActivity extends AppCompatActivity {
             phoneNumber.setError("FIELD CANNOT BE EMPTY");
             return false;
         }
-        else  if (!phoneSignUp.matches("[+][0-9]{10,13}$")) { //WOULD THIS MESS WITH FORMATING OF PHONE NUMBER WE WANT
+        else  if (!phoneSignUp.matches("^[+]?[0-9]{10}$")) { //WOULD THIS MESS WITH FORMATING OF PHONE NUMBER WE WANT
             phoneNumber.requestFocus();
-            phoneNumber.setError("ENTER A VALID PHONE NUMBER");
+            phoneNumber.setError("INVALID PHONE NUMBER");
             return false;
         }
         else if (passwordSignUp.length() < 5) {
             passwordEntered.requestFocus();
-            passwordEntered.setError("MINIMUM 6 CHARACTER REQUIRED");
+            passwordEntered.setError("MINIMUM 6 CHARACTERS REQUIRED");
             return false;
         }
         else if (!passwordSignUp.equals(passwordSignUpConfirm)) {
