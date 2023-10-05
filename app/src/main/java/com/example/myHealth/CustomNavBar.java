833927c23f3348c2c5b3ed2e0d11d3f8495a7309
AnaCastillo23 <anaemily.castilloperez.305@my.csun.edu>
2023-10-05 09:25:21 -0700

    Home Page version 2

---
+++ app/src/main/java/com/example/myHealth/CustomNavBar.java
@@ -0,0 +1,14 @@
+package com.example.myHealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomNavBar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_nav_bar);
+    }
+}
