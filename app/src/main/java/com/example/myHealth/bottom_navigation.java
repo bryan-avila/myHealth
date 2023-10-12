2865c541d3ef97ec4eaad224b7b752848ffc48c4
AnaCastillo23 <anaemily.castilloperez.305@my.csun.edu>
2023-10-11 20:18:09 -0700

    Home page ver 4.0 newer version of the home page

---
+++ app/src/main/java/com/example/myHealth/bottom_navigation.java
@@ -0,0 +1,15 @@
+package com.example.myHealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class bottom_navigation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navigation);
+    }
+}
