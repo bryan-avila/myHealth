939271dc801cecd29d79e09b6921b89121957576
ratherbefishingrightnow <bryan.avila.467@my.csun.edu>
2023-10-17 16:05:06 -0700

    updated med history page, added diet survey page

---
+++ app/src/main/java/com/example/myHealth/first_time_sign_up_diet_survey.java
@@ -0,0 +1,14 @@
+package com.example.myHealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class first_time_sign_up_diet_survey extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_time_sign_up_diet_survey);
+    }
+}
