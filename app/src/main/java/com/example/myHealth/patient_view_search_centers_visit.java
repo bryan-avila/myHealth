28069c8be785718d59a91b7f4e6a98cb7925cba4
ratherbefishingrightnow <bryan.avila.467@my.csun.edu>
2023-10-24 14:59:44 -0700

    changed appointments to visit/ added new page view

---
+++ app/src/main/java/com/example/myHealth/patient_view_search_centers_visit.java
@@ -0,0 +1,14 @@
+package com.example.myHealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class patient_view_search_centers_visit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_view_search_centers_visit);
+    }
+}
