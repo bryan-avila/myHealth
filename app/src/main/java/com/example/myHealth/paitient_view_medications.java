0ccfd0cdc41d5bdf8f019b681e3c9843df9dd49f
ratherbefishingrightnow <bryan.avila.467@my.csun.edu>
2023-10-24 14:35:37 -0700

    added patient medications/diet plan page

---
+++ app/src/main/java/com/example/myHealth/paitient_view_medications.java
@@ -0,0 +1,14 @@
+package com.example.myHealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class paitient_view_medications extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paitient_view_medications);
+    }
+}
