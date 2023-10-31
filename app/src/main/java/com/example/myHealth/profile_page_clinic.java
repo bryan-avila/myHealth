65eb0c5018577e9b34a55a50beb86f27d9211e45
AnaCastillo23 <anaemily.castilloperez.305@my.csun.edu>
2023-10-31 16:09:05 -0700

    Clinic Profile edits

--- app/src/main/java/com/example/myHealth/profile_page_clinic.java
+++ app/src/main/java/com/example/myHealth/profile_page_clinic.java
@@ -6,11 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class profile_page_clinic extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
