aea3efda47f3f758f06fc85a6223c2e9a9bef428
AnaCastillo23 <anaemily.castilloperez.305@my.csun.edu>
2023-10-31 23:08:04 -0700

    Medications Page Displays info about medication

--- app/src/main/java/com/example/myHealth/profile_page_clinic.java
+++ app/src/main/java/com/example/myHealth/profile_page_clinic.java
@@ -8,18 +8,35 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.ListenerRegistration;
 
 public class profile_page_clinic extends AppCompatActivity {
+
+    TextView clinicEmailPlaceholder, clinicNamePlaceholder, clinicLocationPlaceholder, clinicPhonePlaceholder;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseAuth mAuth = myFirestore.getmAuthInstance();
+    FirebaseUser currentUser = mAuth.getCurrentUser();
+    private DocumentReference uRef = db.collection("users").document(currentUser.getUid());
+    private ListenerRegistration userL;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page_clinic);
+
+        clinicEmailPlaceholder = findViewById(R.id.clinicEmailPlaceholder);
+        clinicNamePlaceholder = findViewById(R.id.clinicNamePlaceholder);
+        clinicLocationPlaceholder = findViewById(R.id.clinicLocationPlaceholder);
+        clinicPhonePlaceholder = findViewById(R.id.clinicPhonePlaceholder);
 
         //Initialize and assign variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation2);
@@ -40,7 +57,7 @@ public class profile_page_clinic extends AppCompatActivity {
                     startActivity(new Intent(getApplicationContext(), clinic_home_page.class));
                     finish();
                     return true;
-                } else if (id == R.id.medicalHistId) {
+                } else if (id == R.id.medicalHistIdClinic) {
                     startActivity(new Intent(getApplicationContext(), medical_records_page_clinic.class));
                     finish();
                     return true;
@@ -55,5 +72,9 @@ public class profile_page_clinic extends AppCompatActivity {
             }
 
         });
+
+        //Log out button here
+
+        //Retrieve information from database to display
     }
 }
