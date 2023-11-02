338bf873ce7d1076941a08dad4470f8b74c9be61
AnaCastillo23 <anaemily.castilloperez.305@my.csun.edu>
2023-11-02 15:10:15 -0700

    Edits to clinic view records

---
+++ app/src/main/java/com/example/myHealth/clinic_prescription_form.java
@@ -0,0 +1,15 @@
+package com.example.myHealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class clinic_prescription_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clinic_prescription_form);
+    }
+
+}
--- app/src/main/java/com/example/myHealth/clinic_view_prescriptions.java
+++ app/src/main/java/com/example/myHealth/clinic_view_prescriptions.java
@@ -2,7 +2,9 @@ package com.example.myHealth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class clinic_view_prescriptions extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class clinic_view_prescriptions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clinic_view_prescriptions);
+    }
+
+    public void onFillPrescriptionClick(View view) {
+        startActivity(new Intent(getApplicationContext(), clinic_prescription_form.class));
     }
 }
