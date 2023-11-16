76e98f779322138d3f2714fd8f9aef5d85a33a62
ratherbefishingrightnow <bryan.avila.467@my.csun.edu>
2023-11-16 14:27:25 -0800

    Patient's Home Page Displays Medications in recycler view

--- app/src/main/java/com/example/myHealth/home_page.java
+++ app/src/main/java/com/example/myHealth/home_page.java
@@ -3,9 +3,12 @@ package com.example.myHealth;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,15 +19,19 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class home_page extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -71,6 +78,46 @@ public class home_page extends AppCompatActivity {
                     return true;
                 }
                 return false;
+            }
+        });
+
+        // Set up the RecyclerView
+        RecyclerView med_recycle_view = findViewById(R.id.recycler_view_home_page_medications);
+        med_recycle_view.setLayoutManager(new LinearLayoutManager(this));
+
+        //list of items
+        FirebaseFirestore db = myFirestore.getDBInstance();
+        FirebaseAuth mAuth = myFirestore.getmAuthInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        // Get the correct database path
+        CollectionReference patientMedRef = db.collection("users").document(currentUser.getUid()).collection("prescriptionsInfo");
+
+        patientMedRef.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                List<PrescribedMedications> pMedications_list = new ArrayList<>();
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    PrescribedMedications pMedication = document.toObject(PrescribedMedications.class);
+                    pMedications_list.add(pMedication);
+                }
+                Log.d("TAG", "Medications size: " + pMedications_list.size()); // Check the size of the clinics list
+                // Set the adapter (you'll create and set the adapter in later steps)
+                MyPrescribedMedAdapter myAdapter = new MyPrescribedMedAdapter(getApplicationContext(), pMedications_list);
+                med_recycle_view.setAdapter(myAdapter);
+
+               /* myAdapter.setOnItemClickListener(new MyPrescribedMedAdapter().OnItemClickListener() {
+                    //this sends the user to the clinic's specific appointment page
+                    @Override
+                    public void onItemClick(int position, PrescribedMedications pMedications) {
+                        // Handle the item click here
+                        Intent intent = new Intent(patient_view_prescribed_med.this, appointments_page.class);
+                        startActivity(intent);
+                    }
+                });
+*/
+            }
+            else {
+                // Handle the error
+                Log.e("TAG", "Error getting medications", task.getException());
             }
         });
 
