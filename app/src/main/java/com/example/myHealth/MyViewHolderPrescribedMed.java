f24fbf8c745b3e1ab613c944f39c8f78a6c80522
JomkhA <akkharaw.jomkhanthiphol.179@my.csun.edu>
2023-11-16 16:02:15 -0800

    Minor changes to daily medication recyclerview

--- app/src/main/java/com/example/myHealth/MyViewHolderPrescribedMed.java
+++ app/src/main/java/com/example/myHealth/MyViewHolderPrescribedMed.java
@@ -1,5 +1,6 @@
 package com.example.myHealth;
 
+import android.graphics.Paint;
 import android.view.View;
 import android.widget.TextView;
 
@@ -14,8 +15,11 @@ public class MyViewHolderPrescribedMed extends RecyclerView.ViewHolder {
         super(itemView);
         // Get the .XML R.id's from the prescribed_med_item_view.xml file
         medicationName = itemView.findViewById(R.id.text_view_prescribed_med_name);
-        frequency = itemView.findViewById(R.id.text_view_prescribed_dosage);
-        dosageAmount = itemView.findViewById(R.id.text_view_prescribed_frequency);
+        frequency = itemView.findViewById(R.id.text_view_prescribed_frequency);
+        dosageAmount = itemView.findViewById(R.id.text_view_prescribed_dosage);
+
+        // Underline medication name
+        medicationName.setPaintFlags(medicationName.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
     }
 
 
