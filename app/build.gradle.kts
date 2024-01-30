0983484208494c09cbeefe1091de057b53ae3f47
AnaCastillo23 <89889778+AnaCastillo23@users.noreply.github.com>
2024-01-30 15:43:12 -0800

    minor changes to diet diary

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.myHealth"
-        minSdk = 26
+        minSdk = 28
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
