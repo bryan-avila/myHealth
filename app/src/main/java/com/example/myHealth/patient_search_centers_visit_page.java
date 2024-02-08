92929a16b3b0350a7e50d4ad5460e994ce8e2bac
JomkhA <akkharaw.jomkhanthiphol.179@my.csun.edu>
2024-02-08 14:10:49 -0800

    nutrition tab stuff lol

--- app/src/main/java/com/example/myHealth/patient_search_centers_visit_page.java
+++ app/src/main/java/com/example/myHealth/patient_search_centers_visit_page.java
@@ -58,7 +58,7 @@ public class patient_search_centers_visit_page extends AppCompatActivity {
                     finish();
                     return true;
                 } else if (id == R.id.resourcesId) {
-                    startActivity(new Intent(getApplicationContext(), patient_diet_page.class));
+                    startActivity(new Intent(getApplicationContext(), patient_nutrition_page.class));
                     finish();
                     return true;
                 } else if (id == R.id.profileId) {
