075e0fa1f2c91f4d3fb1e3c358afe1dfb18c4e9b
ratherbefishingrightnow <bryan.avila.467@my.csun.edu>
2024-02-08 13:44:29 -0800

    Fixed Patient Edit Profile crash

--- app/src/main/java/com/example/myHealth/patient_edit_my_profile.java
+++ app/src/main/java/com/example/myHealth/patient_edit_my_profile.java
@@ -4,7 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class patient_edit_my_profile extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseAuth mAuth = MyFirestore.getmAuthInstance();
+    FirebaseUser currentUser = mAuth.getCurrentUser();
+    private DocumentReference userRef = db.collection("users").document(currentUser.getUid());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
