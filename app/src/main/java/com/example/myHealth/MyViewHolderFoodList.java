3ffdb74a7d9992926d6257a41f1791e234634b69
ratherbefishingrightnow <bryan.avila.467@my.csun.edu>
2024-02-22 14:59:28 -0800

    Set up FoodList Adapter and FoodList Holder

---
+++ app/src/main/java/com/example/myHealth/MyViewHolderFoodList.java
@@ -0,0 +1,14 @@
+package com.example.myHealth;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyViewHolderFoodList extends RecyclerView.ViewHolder {
+
+    TextView foodName;
+
+    public MyViewHolderFoodList(@NonNull View itemView){
+        super(itemView);
+
+    }
+}
