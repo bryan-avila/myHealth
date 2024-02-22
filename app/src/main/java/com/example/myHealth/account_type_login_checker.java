ac0568359a29173882b391bc92490b23e02ac6a2
AnaCastillo23 <89889778+AnaCastillo23@users.noreply.github.com>
2024-02-22 15:53:10 -0800

    making user checker before log in

---
+++ app/src/main/java/com/example/myHealth/account_type_login_checker.java
@@ -0,0 +1,28 @@
+package com.example.myHealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class account_type_login_checker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_type_login_checker);
+    }
+
+    public void onPatientClickChecker(View view) {
+        Intent intent = new Intent(getApplicationContext(), main_login_page.class);
+        startActivity(intent);
+        finish(); // cannot press back
+    }
+
+    public void onCaretakerClickChecker(View view) {
+        Intent intent = new Intent(getApplicationContext(), main_login_page.class);
+        startActivity(intent);
+        finish(); // cannot press back
+    }
+}
