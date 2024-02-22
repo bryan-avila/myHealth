623a21ad10e48399a11eb6457aa5dabf5d6958c2
ratherbefishingrightnow <bryan.avila.467@my.csun.edu>
2024-02-22 14:34:18 -0800

    Commented a potential way to get single item nutrients

--- app/src/main/java/com/example/myHealth/patient_dietary_plan_page.java
+++ app/src/main/java/com/example/myHealth/patient_dietary_plan_page.java
@@ -14,6 +14,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
 
 public class patient_dietary_plan_page extends AppCompatActivity {
 
@@ -31,7 +32,10 @@ public class patient_dietary_plan_page extends AppCompatActivity {
                 try {
                     Log.d("start", "start");
                     // USDA FoodData Central API endpoint for food search
+                    //https://api.nal.usda.gov/fdc/v1/food/2646171?nutrients=203&nutrients=204&nutrients=205
+                   // String endpoint = "https://api.nal.usda.gov/fdc/v1/food/2346409?api_key=hIXmsCYannc5plOrGfwlqSZkuUsBAznpJEgxtz5T";
                     String endpoint = "https://api.nal.usda.gov/fdc/v1/foods/search?query=apple&api_key=DEMO_KEY";
+
 
                     // Create a URL object
                     URL url = new URL(endpoint);
@@ -59,7 +63,35 @@ public class patient_dietary_plan_page extends AppCompatActivity {
                     }
                     reader.close();
 
+//                    Scanner scanner = new Scanner(connection.getInputStream());
+//                    StringBuilder response = new StringBuilder();
+//                    while (scanner.hasNextLine()) {
+//                        response.append(scanner.nextLine());
+//                    }
+//                    scanner.close();
+
+                    //****** TESTING FOR ONE FOOD INSTEAD OF LIST OF FOODS***********
                     // Parse the JSON response
+//                    JSONParser parser = new JSONParser();
+//                    JSONObject jsonResponse = (JSONObject) parser.parse(response.toString());
+//                   // JSONObject foodData = (JSONObject) parser.parse(response.toString());
+
+
+//                    // Access the list of foods/nutrients
+//                    JSONArray nutrients = (JSONArray) foodData.get("foodNutrients");
+//                    for (Object nutrientObj : nutrients) {
+//                       JSONObject foodObject = (JSONObject) food;
+//                       // JSONObject nutrient = (JSONObject) nutrientObj;
+//                      //  System.out.println("Food Name: " + foodObject.get("foodNutrients"));
+//                      //  food_name = "Food Name: " + foodObject.get("foodNutrients");
+//                        //String nutrientName = (String) nutrient.get("name").toString();
+//                        //System.out.println(nutrientName);
+//                        Log.d("happy", "worked");
+//                        // Access other properties as needed
+//                    }
+                    //****** TESTING FOR ONE FOOD INSTEAD OF LIST OF FOODS***********
+
+
                     JSONParser parser = new JSONParser();
                     JSONObject jsonResponse = (JSONObject) parser.parse(response.toString());
 
@@ -67,15 +99,15 @@ public class patient_dietary_plan_page extends AppCompatActivity {
                     JSONArray foods = (JSONArray) jsonResponse.get("foods");
                     for (Object food : foods) {
                         JSONObject foodObject = (JSONObject) food;
-                        System.out.println("Food Name: " + foodObject.get("description"));
                         food_name = "Food Name: " + foodObject.get("description");
+                        System.out.println("Food Name: " + foodObject.get("description"));
                         Log.d("happy", "worked");
                         // Access other properties as needed
                     }
 
                     runOnUiThread(() -> {
                                 // Update the TextView text
-                                food_text.setText(food_name);
+                               food_text.setText(food_name);
                             });
 
                     // Disconnect the connection
